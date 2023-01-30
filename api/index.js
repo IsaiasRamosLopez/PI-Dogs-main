374b8e721876291d4b46d6957b3ac91db30cf59f
Isaias R. Lopez <isaias.ramos.27@unsch.edu.pe>
2023-01-30 08:48:37 -0500

    ------------------------------

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const { conn } = require("./src/db.js");
 const PORT = process.env.PORT || 5000;
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
 	server.listen(PORT, () => {
 		console.log(`server listening on port ${PORT}`); // eslint-disable-line no-console
 	});
